0a002bdecdb18ec1b0c21389f3f4bf73120f8c11
Derik Oliveira <derik.oliveira@sensedia.com>
2023-03-22 10:20:50 -0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  trailingComma: 'none',
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from '@/app'
-import './index.css'
+import { App } from '@/app';
+import { createRoot } from 'react-dom/client';
 
-const rootElement = document.querySelector('[data-js="root"]')
+const rootElement = document.querySelector('[data-js="root"]');
 
 if (!rootElement) {
-  throw new Error('Failed to find the root element')
+  throw new Error('Failed to find the root element');
 }
 
-const root = createRoot(rootElement)
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(rootElement);
+root.render(<App />);
