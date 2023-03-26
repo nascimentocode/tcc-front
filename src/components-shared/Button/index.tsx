d87f7e8cf58894779d9e20971bc0975c95471e71
Derik Oliveira <derik.oliveira@sensedia.com>
2023-03-26 11:23:35 -0300

    fix: fixing lint error

--- src/components-shared/Button/index.tsx
+++ src/components-shared/Button/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import classnames from 'classnames';
+import React from 'react';
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary' | 'outlined';
--- src/components-shared/Input/index.tsx
+++ src/components-shared/Input/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import classnames from 'classnames';
+import React from 'react';
 
 interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
