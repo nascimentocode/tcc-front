8a0fbd73c686a0c50aaebf282b6eb2183c33cfba
Derik Oliveira <derik.oliveira@sensedia.com>
2023-03-26 11:20:29 -0300

    fix: removing auto import

--- src/components-shared/Input/index.tsx
+++ src/components-shared/Input/index.tsx
@@ -1,5 +1,5 @@
-import classnames from 'classnames';
 import React from 'react';
+import classnames from 'classnames';
 
 interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
